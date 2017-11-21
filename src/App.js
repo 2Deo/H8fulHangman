0d6db2509ea0780b3b60f77bb471de21cea4d5d8
2Deo <ziemowit.holda@gmail.com>
2017-11-21 12:18:23 +0100

    Add hangman

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import HiddenText from './components/HiddenText'
 import Alphabet from './components/Alphabet'
+import Hangman from './components/Hangman'
 
 class App extends Component {
   constructor(props) {
@@ -10,22 +11,30 @@ class App extends Component {
     this.state = {
       question: "The favourite food of Ali",
       text: "Soy beans",
-      guesses: []
+      guesses: [],
+      lives: 7
     }
   }
 
   checkLetter(letter) {
-    const {guesses} = this.state
-    this.setState({guesses: [...guesses, letter]})
+    const {text, guesses, lives} = this.state
+
+    if (lives > 0) {
+      this.setState({
+        guesses: [...guesses, letter],
+        lives: text.toLowerCase().indexOf(letter) > -1 ? lives : lives -1
+      })
+    }
   }
 
   render() {
-    const {question, text, guesses } = this.state
+    const {question, text, guesses, lives } = this.state
 
     return (
       <div>
           <img src={logo} alt="H8ful Hangman" />
           <HiddenText question={question} text={text} guesses={guesses} />
+          <Hangman lives={lives}/>
           <Alphabet onLetterClick={this.checkLetter.bind(this)} guesses={guesses}/>
       </div>
     )
