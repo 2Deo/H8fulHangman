aa99b8087de2d36c42aee18f12a94a4e4664dc4a
2Deo <ziemowit.holda@gmail.com>
2017-11-21 11:30:36 +0100

    Add Alphabet, buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import HiddenText from './components/HiddenText'
+import Alphabet from './components/Alphabet'
 
 class App extends Component {
   constructor(props) {
@@ -8,9 +9,14 @@ class App extends Component {
 
     this.state = {
       question: "The favourite food of Ali",
-      text: "Soya beans"
+      text: "Soy beans"
     }
   }
+
+  checkLetter(letter) {
+    console.log(letter)
+  }
+
   render() {
     const {question, text } = this.state
 
@@ -18,8 +24,9 @@ class App extends Component {
       <div>
           <img src={logo} alt="H8ful Hangman" />
           <HiddenText question={question} text={text} />
+          <Alphabet onLetterClick={this.checkLetter.bind(this)}/>
       </div>
-    );
+    )
   }
 }
 
